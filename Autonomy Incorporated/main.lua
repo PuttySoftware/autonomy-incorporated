e311f2ca5c05facef1421d35f487a9765d53e4c6
Eric Ahnell <eric.ahnell@puttysoftware.com>
2018-09-03 10:35:28 -0400

    Make it possible to exit the game with ESC.

--- Autonomy Incorporated/main.lua
+++ Autonomy Incorporated/main.lua
@@ -1,3 +1,9 @@
 function love.draw()
     love.graphics.print('There will be a game here eventually', 400, 300)
 end
+
+function love.update(dt)
+  if love.keyboard.isDown('escape') then
+    love.event.push('quit')
+  end
+end
