7172bba3b9a2335cb1c4a9521ace07fcd115fc72
Eric Ahnell <eric.ahnell@puttysoftware.com>
2018-09-09 11:23:43 -0400

    Merge music and OGG sounds. Add localization.

---
+++ Autonomy Incorporated/assets/lang/en_us.lua
@@ -0,0 +1,12 @@
+return {
+	locale = "en_us",
+	base   = "assets/sounds/en_us",
+	quotes = { "\"", "\"" },
+	strings = {
+		-- Main Menu
+		["main/play"]    = { text = "Play" },
+		["main/options"] = { text = "Options" },
+		["main/credits"] = { text = "Credits" },
+		["main/exit"]    = { text = "Exit" },
+	}
+}
